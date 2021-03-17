386a32662b57e82df2253561eeec6502ac403a1b
VISHAL RATHORE <58523371+VISHALR1021@users.noreply.github.com>
2021-03-17 19:47:11 +0500

    changes 17/3
    I have changed, discover page and hide he search bar also

--- front-end/src/components/Discover.js
+++ front-end/src/components/Discover.js
@@ -7,6 +7,8 @@ import one from '../styles/images/1.jpg'
 import two from '../styles/images/2.jpg'
 import three from '../styles/images/5.png'
 import four from '../styles/images/4.jpg'
+import graphic from '../styles/images/55.jpg'
+import profile from '../styles/images/profile.svg';
 function Discover() {
 
     const [projects,setProjects] = useState([])
@@ -39,9 +41,41 @@ function Discover() {
 
     const projectList = projects.map((project) =>{
         return(
-            <div class="col-3">
-            <Link to={"/project/"+project.projectId}><img src={project.projectThumbnail} alt="" /><h4>{project.title}</h4></Link>
+
+            
+    <div class="posts">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                            <Link class="project" to={"/project/"+project.projectId}>
+                                <div class="project-post">
+                                    <div class="project-img">
+                                        <img class="project-img" src={project.projectThumbnail} alt="" width="400px" style={{margin:"0px"}}/>
+                                    </div>
+                                    <div class="project-content">
+                                        <h2 class="project-title" style={{fontSize: "1.2rem", position:"relative"}}>{project.title}</h2>
+                                    </div>
+                                </div>
+                            <div class="project-header">
+                                    <div class="profile-img">
+                                        <img src={profile} alt="" class="profile-image" style={{width: "24px", height: "24px"}} />
+                                    </div>
+                                    <div class="profile-nav-info">
+                                        <h2 class="username" style={{fontSize: "1rem", fontWeight: "500", padding: "none"}}>Vishal Rathore</h2>
+                                    </div>
+                            </div>
+{/*                             <div class="project-ratings" style={{justifyContent: "right", textAlign: "right", alignItems: "right", width: "100%", padding:"2px 2px"}}>
+                                <span class="ratings" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}><i class="fa fa-star" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;0</span>
+                                <span class="project-reviews" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-eye" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;21</span>
+                                <span class="project-likes" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-heart-o" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;2.4k</span>
+                            </div> */}
+                        </Link>
+                </div>
+
+                </div>
+            </div>
         </div>
+        
         )
     })
 
@@ -87,90 +121,41 @@ function Discover() {
                 </div> 
          </div>
 
-    <Searchbar></Searchbar>  
+{/*     <Searchbar></Searchbar>   */}
 
     <div class="posts">
         <div class="small-container">
             <div class="row">
                 <div class="col-3">
-                            <a class="project" href="./project.html">
+                            <Link class="project" to="/Project">
                                 <div class="project-post">
                                     <div class="project-img">
-                                        <img class="project-img" src="./styles/images/graphic desgin.png" alt="" width="400px" style={{margin:"0px"}}/>
+                                        <img class="project-img" src={graphic} alt="" width="400px" style={{margin:"0px"}}/>
                                     </div>
                                     <div class="project-content">
-                                        <h3 class="project-title" style={{fontSize: "1rem", position: "absolute", top: "20px", left: "20px"}}>Peace</h3>
+                                        <h2 class="project-title" style={{fontSize: "1.2rem", position: "relative"}}>Peace</h2>
                                     </div>
                                 </div>
                             <div class="project-header">
                                     <div class="profile-img">
-                                        <img src="./styles/images/profile.svg" alt="" class="profile-image" style={{width: "24px", height: "24px"}} />
+                                        <img src={profile} alt="" class="profile-image" style={{width: "24px", height: "24px"}} />
                                     </div>
                                     <div class="profile-nav-info">
                                         <h2 class="username" style={{fontSize: "1rem", fontWeight: "500", padding: "none"}}>Vishal Rathore</h2>
                                     </div>
                             </div>
-                            <div class="project-ratings" style={{justifyContent: "right", textAlign: "right", alignItems: "right", width: "100%", padding:"2px 2px"}}>
-
-                                        
-                                        <span class="ratings" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}><i class="fa fa-star" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;0</span>                      
-                                
+{/*                             <div class="project-ratings" style={{justifyContent: "right", textAlign: "right", alignItems: "right", width: "100%", padding:"2px 2px"}}>
+                                <span class="ratings" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}><i class="fa fa-star" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;0</span>
                                 <span class="project-reviews" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-eye" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;21</span>
                                 <span class="project-likes" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-heart-o" style={{color: "#7e27cf", fontSize: "1rem", fontWeight: "600", marginRight: "4px"}}></i>&nbsp;2.4k</span>
-                            </div>
-                        </a>
+                            </div> */}
+                        </Link>
                 </div>
-                <div class="col-3">
-                    <a class="project" href="./project.html">
-                        <div class="project-post">
-                            <div class="project-img">
-                                <img class="project-img" src="./styles/images/graphic desgin.png" alt="" width="400px" style={{margin: "0px"}} />
-                            </div>
-                            <div class="project-content">
-                                <h3 class="project-title" style={{fontSize: "1rem", position: "absolute", top: "20px", left: "20px"}}>Peace</h3>
-                            </div>
-                        </div>
-                    <div class="project-header">
-                            <div class="profile-img">
-                                <img src="./styles/images/profile.svg" alt="" class="profile-image" style={{width: "24px", height: "24px"}} />
-                            </div>
-                            <div class="profile-nav-info">
-                                <h2 class="username" style={{fontSize: "1rem", fontWeight: "500", padding: "none"}}>Vishal Rathore</h2>
-                            </div>
-                    </div>
-                    <div class="project-ratings" style={{justifyContent: "right", textAlign: "right", alignItems: "right", width: "100%", padding:"2px 2px"}}>
-                        <span class="project-reviews" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-eye" style={{color: "#7e27cf", fontSize: "1ren", fontWeight: "600"}}></i>&nbsp;21</span>
-                        <span class="project-likes" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-heart-o" style={{color: "#7e27cf", fontSize: "1ren", fontWeight: "600"}}></i>&nbsp;2.4k</span>
-                    </div>
-                </a>
-                </div>
-                <div class="col-3">
-                    <a class="project" href="./project.html">
-                        <div class="project-post">
-                            <div class="project-img">
-                                <img class="project-img" src="./styles/images/graphic desgin.png" alt="" width="400px" style={{margin: "0px"}} />
-                            </div>
-                            <div class="project-content">
-                                <h3 class="project-title" style={{fontSize: "1rem", position: "absolute", top: "20px", left: "20px"}}>Peace</h3>
-                            </div>
-                        </div>
-                    <div class="project-header">
-                            <div class="profile-img">
-                                <img src="./styles/images/profile.svg" alt="" class="profile-image" style={{width: "24px", height: "24px"}} />
-                            </div>
-                            <div class="profile-nav-info">
-                                <h2 class="username" style={{fontSize: "1rem", fontWeight: "500", padding:" none"}}>Vishal Rathore</h2>
-                            </div>
-                    </div>
-                    <div class="project-ratings" style={{justifyContent: "right", textAlign: "right", alignItems: "right", width: "100%", padding:"2px 2px"}}>
-                        <span class="project-reviews" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-eye" style={{color: "#7e27cf", fontSize: "1ren", fontWeight: "600"}}></i>&nbsp;21</span>
-                        <span class="project-likes" style={{fontSize: ".8rem", color: "gray", fontWeight: "500"}}>&nbsp;<i class="fa fa-heart-o"style={{color: "#7e27cf", fontSize: "1ren", fontWeight: "600"}}></i>&nbsp;2.4k</span>
-                    </div>
-                </a>
+
                 </div>
             </div>
         </div>
-    </div>
+
     <div class="foot">
         <p>All &copy; USERVICE | Designed & Developed by Vishal Rathore, Usman Hussain & Hussain Zohair</p>
 
